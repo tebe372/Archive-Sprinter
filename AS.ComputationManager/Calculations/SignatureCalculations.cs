b5eb9abecc4670dd65f5f20b71b8c681e7e58baf
tebe372 <tebebeyesusgaredew@gmail.com>
2020-02-19 15:39:14 -0800

    added a signature calculation example.

---
+++ AS.ComputationManager/Calculations/SignatureCalculations.cs
@@ -0,0 +1,17 @@
+﻿using AS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS.ComputationManager.Calculations
+{
+    public static class SignatureCalculations
+    {
+        public static double Variance(List<Signal> e)
+        {
+            return 0d;
+        }
+    }
+}
--- AS.Config/SignatureSetting.cs
+++ AS.Config/SignatureSetting.cs
@@ -1,4 +1,5 @@
-﻿using AS.Core.Models;
+﻿using AS.ComputationManager.Calculations;
+using AS.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,17 @@ using System.Threading.Tasks;
 
 namespace AS.Config
 {
-    public class SignatureSetting
+    public abstract class SignatureSetting
     {
-        public void Process(List<Signal> e)
+        public abstract void Process(List<Signal> e);
+    }
+    public class Variance : SignatureSetting
+    {
+        public override void Process(List<Signal> e)
         {
-
+            //according to the input channels in variance, take part of the e as input to the following function call.
+            var d = e.Take(5); // just an example
+            var variance = SignatureCalculations.Variance(e);
         }
     }
 }
